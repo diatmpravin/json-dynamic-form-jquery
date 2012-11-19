501670f3ca47843f096a0c3c75762034a29b40de
diatmpravin <diatm.pravin.it.07.27@gmail.com>
2012-11-19 23:52:28 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonDynamicFormJquery::Application.config.secret_token = '41207a94be4c6f85b5df40cc2d153f3c7e901f986fc1eb77b7ebbd24b8bc216560d3fb4f7ac17bff039f1a6a54f2481d471955f1476a876fe8fe03ff40260bd8'
